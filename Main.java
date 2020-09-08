a40ad13233388d9da5a3f5ee5951f876e6c00fc8
Shamiltestgrid <60189855+Shamiltestgrid@users.noreply.github.com>
2020-09-08 16:54:10 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
 public class Main {
-        public static void main(String[] args) {
-          int amount = 100;
-          int transfer = 1100;
-          int limit = 1000;        
-          int bonus;
-  
-          if(transfer < limit) {
-          bonus = 0;
-          } else {
-          bonus = transfer / 100;
-          }
-        
-         System.out.println(bonus);
-          
-         int totalAmount = amount + transfer + bonus;
-         System.out.println(totalAmount);
+  public static void main(String[] args) {
+    int amount = 100;
+    int transfer = 1100;
+    int limit = 1000;
+    int bonus;
+
+    if (transfer < limit) {
+      bonus = 0;
+    } else {
+      bonus = transfer / 100;
     }
+
+    System.out.println(bonus);
+
+    int totalAmount = amount + transfer + bonus;
+    System.out.println(totalAmount);
+  }
+}
